3be7206624f5672622a95b8ba168149c67407da2
Luis <lacarbonel2017@gmail.com>
2021-03-18 00:48:54 -0300

    initial-structure
    To complete with other services.

---
+++ pepeganga-api/cloud-gateway-service/src/test/java/uy/com/pepeganga/api/cloud/gateway/service/CloudGatewayServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package uy.com.pepeganga.api.cloud.gateway.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudGatewayServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
